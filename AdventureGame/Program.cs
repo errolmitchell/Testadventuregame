e70151f25bd9fdd0334a93b4c50693aecd6c3e13
Errol Mitchell <errolbmitchell@gmail.com>
2022-09-11 19:51:20 -0400

    fist commit

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            string CharacterName = "";
+
+            Console.WriteLine("The Adventure Game!");
+            Console.WriteLine("Welcome you brave soul...This Game will test all your skills");
+            Console.ReadKey();
+            Console.WriteLine("nunchuck skills...bowstaff skills...");
+            Console.ReadKey();
+            Console.WriteLine("Chances are..you won't make it out of this adventure alive");
+            Console.ReadKey();
+            Console.WriteLine("Type your name so we can put it on your headstone when you don't survive this adventure");          
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Welcome," + CharacterName + "...Prepare for awesomeness");
+            Console.ReadKey();
+
+            
+                      
+
+        }
+    }
+}
